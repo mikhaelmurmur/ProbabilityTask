feea3d40fc711760c16819ebb6b79ccd399e838b
Mikhaelmurmur <stolovich@yahoo.com>
2015-10-17 21:01:10 +0300

    Solved task

---
+++ CheckHomeTask/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckHomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int betta = 2;
+            double probability = 0.0;
+            while (probability < 0.5)
+            {
+                probability = (Math.Pow(6, betta - 1) - Math.Pow(5, betta - 1)) /
+                              (6 * (Math.Pow(6, betta) - 35 * (Math.Pow(6, betta - 2) - Math.Pow(5, betta - 2))));
+                betta++;
+            }
+            Console.WriteLine(betta - 1);
+        }
+    }
+}
